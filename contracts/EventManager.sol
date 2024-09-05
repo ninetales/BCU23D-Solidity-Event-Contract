0073feab495774f15cd7a6a3deee0c61d443f301
N1N3T4L3S <6765614+ninetales@users.noreply.github.com>
2024-09-05 20:28:45 +0200

    :tada: first contract ever!

---
+++ contracts/EventManager.sol
@@ -0,0 +1,255 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+contract EventManager is ReentrancyGuard{
+    address private owner;
+    uint16 private eventsCounter;
+    uint256 private refundTimespan;
+    string[] private eventsIdList;
+
+    constructor() {
+        owner = msg.sender;
+        eventsCounter = 0;
+        refundTimespan = 86400;
+    }
+
+    // ==========================================================================================
+    // Errors
+    // ==========================================================================================
+    error NoTicketFound(address owner, string eventId);
+    error NoEventFound(string eventId);
+
+    // ==========================================================================================
+    // Enums
+    // ==========================================================================================
+    enum EventStatus { Active, Paused }
+
+    // ==========================================================================================
+    // Structs
+    // ==========================================================================================
+    struct Ticket {
+        address owner;
+        string fname;
+        string lname;
+        string email;
+        uint256 paidPrice;
+        uint256 purchased;
+    }
+
+    struct Event {
+        string eventId;
+        address creator;
+        string name;
+        uint16 ticketLimit;
+        uint256 eventDate;
+        uint256 price; // Stored in wei
+        EventStatus status;
+        Ticket[] tickets;
+    }
+
+    // ==========================================================================================
+    // Modifiers
+    // ==========================================================================================
+    modifier admin() {
+        require(msg.sender == owner, "Access denied! You must be the owner of this contract.");
+        _;
+    }
+
+    // ==========================================================================================
+    // Maps
+    // ==========================================================================================
+    mapping(string => Event) internal events;
+
+    // ==========================================================================================
+    // Events
+    // ==========================================================================================
+    event NewEventCreated(string eventId, string eventName, address eventCreator, uint256 eventDate, EventStatus status);
+    event EventStatusUpdated(string eventId, string eventName, address eventCreator, EventStatus status);
+    event NewTicketCreated(address accountAddress, string eventId, uint256 ticketPrice);
+    event TicketCanceled(address accountAddress, string eventId, uint256 refundedAmount);
+    event Log(address sender, bytes data);
+
+    // ==========================================================================================
+    // Functions
+    // ==========================================================================================
+    function createEvent(string memory _name, uint16 _ticketLimit, uint256 _priceInEther, uint256 _eventDate) external admin {
+
+        require(block.timestamp < _eventDate, "Cannot set a timestamp in the past. Try again with a UNIX Timestamp in the future.");
+
+        eventsCounter++;
+        string memory _eventId = string(abi.encodePacked("ev", Strings.toString(eventsCounter)));
+        uint256 _priceInWei = _priceInEther * 1 ether;
+
+        Event storage newEvent = events[_eventId];
+        newEvent.eventId = _eventId;
+        newEvent.creator = msg.sender;
+        newEvent.name = _name;
+        newEvent.ticketLimit = _ticketLimit;
+        newEvent.price = _priceInWei;
+        newEvent.eventDate = _eventDate;
+        newEvent.status = EventStatus.Active;
+
+        eventsIdList.push(_eventId);
+
+        emit NewEventCreated(_eventId, _name, msg.sender, _eventDate, EventStatus.Active);
+
+    }
+
+    // **********************************************
+    // List event IDs
+    // **********************************************
+    function listEvents() public view returns(string[] memory) {
+        return eventsIdList;
+    }
+
+    // **********************************************
+    // Display event details
+    // **********************************************
+    function showEventDetails(string memory _eventId) public view returns (
+        string memory eventId,
+        address creator,
+        string memory name,
+        uint16 ticketLimit,
+        uint256 price,
+        uint256 eventDate,
+        EventStatus status
+    ) {
+        Event storage eventData = events[_eventId];
+        if(eventData.creator == address(0)){
+            revert NoEventFound(_eventId);
+        }
+        return (
+            eventData.eventId,
+            eventData.creator,
+            eventData.name,
+            eventData.ticketLimit,
+            eventData.price,
+            eventData.eventDate,
+            eventData.status
+        );
+    }
+
+    // **********************************************
+    // List participants for an event
+    // **********************************************
+    function listEventParticipants(string memory _eventId) public view admin returns (Ticket[] memory) {
+        require(bytes(_eventId).length > 0, "Event ID cannot be empty");
+        Event storage eventData = events[_eventId];
+        return eventData.tickets;
+    }
+
+    // **********************************************
+    // Toggle event pause
+    // **********************************************
+    function togglePauseEventRegistration(string memory _eventId, EventStatus _status) public admin {
+        Event storage eventData = events[_eventId];
+        require(eventData.creator != address(0), "No event with that ID was found");
+        require(eventData.status != _status, "That status is already set");
+        eventData.status = _status;
+        emit EventStatusUpdated(_eventId, eventData.name, eventData.creator, _status);
+    }
+
+    // **********************************************
+    // Buy a ticket for an event
+    // **********************************************
+    function buyTicket(string memory _eventId, string memory _fname, string memory _lname, string memory _email) external payable nonReentrant {
+
+        Event storage eventData = events[_eventId];
+        (bool ticketExist, ,) = getUserTicket(_eventId);
+
+        require(eventData.creator != address(0), "No event with that ID was found!");
+        require(msg.sender != eventData.creator, "You cannot buy a ticket for your own event");
+        require(!ticketExist, "You have already purchased a ticket. You cannot buy another one..");
+        require(eventData.eventDate > block.timestamp, "Too late to buy a ticket! Better luck next time.");
+        require(eventData.status != EventStatus.Paused, "The ticket sales has been paused. Please try again later.");
+        require(eventData.tickets.length < eventData.ticketLimit, "Tickets are sold out");
+        require(msg.value >= eventData.price, "Not enough funds to buy a ticket! Refill account and please try again.");
+
+        Ticket memory newTicket = Ticket({
+            owner: msg.sender,
+            fname: _fname,
+            lname: _lname,
+            email: _email,
+            paidPrice: eventData.price,
+            purchased: block.timestamp
+        });
+
+        eventData.tickets.push(newTicket);
+        emit NewTicketCreated(msg.sender, eventData.eventId, eventData.price);
+        
+
+        // Refund excess funds
+        if(msg.value > eventData.price){
+            payable(msg.sender).transfer(msg.value - eventData.price);
+        }
+
+    }
+
+    // **********************************************
+    // Get the user ticket
+    // **********************************************
+    function getUserTicket(string memory _eventId) public view returns (bool, Ticket memory, uint16){
+
+        Event storage eventData = events[_eventId];
+        
+        if(eventData.creator == address(0)){
+            return (false, Ticket(address(0), "", "", "", 0, 0), 0); 
+        }
+
+        for(uint16 i = 0; i < eventData.tickets.length; i++){
+            if(eventData.tickets[i].owner == msg.sender){
+                return (
+                    true,
+                    eventData.tickets[i],
+                    i
+                );
+            }
+        } 
+
+        return (false, Ticket(address(0), "", "", "", 0, 0), 0); 
+
+    }
+
+    // **********************************************
+    // Cancel user ticket for an event
+    // **********************************************
+    function cancelTicket(string memory _eventId) public {
+        
+        (bool ticketExist, Ticket memory _userTicket, uint16 _currentTicketIndex) = getUserTicket(_eventId);
+        if(!ticketExist){
+            revert NoTicketFound(msg.sender, _eventId);
+        }
+
+        Event storage eventData = events[_eventId];
+        Ticket[] storage tickets = eventData.tickets;
+        uint256 refundDeadline = eventData.eventDate - refundTimespan;
+        require(block.timestamp <= refundDeadline, "Sorry, the last date for a refund have passed :(");
+
+        uint256 originalArrayLength = tickets.length;
+        tickets[_currentTicketIndex] = tickets[tickets.length -1];
+        tickets.pop();
+        
+        // Assert that the array length has decreased by exactly one
+        assert(originalArrayLength == tickets.length + 1);
+
+        payable(msg.sender).transfer(_userTicket.paidPrice);
+        emit TicketCanceled(msg.sender, _eventId, _userTicket.paidPrice);
+    }
+
+    // **********************************************
+    // Get the contract balance
+    // **********************************************
+    function getContractBalance() public view admin returns(uint){
+        return address(this).balance;
+    }
+
+    // **********************************************
+    // Fallback
+    // **********************************************
+    fallback() external { 
+        emit Log(msg.sender, msg.data);
+    }
+}
---
+++ test/EventManager.ts
@@ -0,0 +1,323 @@
+import { expect } from 'chai';
+import { ethers, network } from 'hardhat';
+import hre from 'hardhat';
+
+describe('EventManager', function () {
+
+    let eventName = "Event-1";
+    let ticketLimit = 1;
+    let priceInEther = 1;
+    let timestampInFuture = 1758287340;
+
+    let eventId = "ev1";
+    let fname = "John";
+    let lname = "Connor";
+    let email = "john.connor@terminator.io";
+    let walletBalance = 100;
+
+
+
+    async function deployContractFixture() {
+        const [owner, addr1, addr2] = await ethers.getSigners();
+        const EventManager = await hre.ethers.getContractFactory('EventManager');
+        const eventManager = await EventManager.deploy();
+        return { eventManager, owner, addr1, addr2 };
+    };
+
+    describe('createEvent', async function () {
+        it('should emit "NewEventCreated" when a an event was created', async () => {
+            const { eventManager } = await deployContractFixture();
+            await expect(eventManager.createEvent(eventName, ticketLimit, priceInEther, timestampInFuture)).to.emit(eventManager, 'NewEventCreated');
+        });
+
+        it('should produce an error when timestamp is set in the past', async () => {
+            const timestampInPast = 0;
+            const { eventManager } = await deployContractFixture();
+            await expect(eventManager.createEvent(eventName, ticketLimit, priceInEther, timestampInPast)).to.be.revertedWith("Cannot set a timestamp in the past. Try again with a UNIX Timestamp in the future.");
+        });
+    });
+
+    describe('listEvents', async function () {
+        it('should return an empty array', async () => {
+            const { eventManager } = await deployContractFixture();
+            expect(await eventManager.listEvents()).to.be.empty;
+        });
+
+        it('should return an array with one event', async () => {
+            const { eventManager } = await deployContractFixture();
+            await eventManager.createEvent(eventName, ticketLimit, priceInEther, timestampInFuture);
+            expect(await eventManager.listEvents()).to.be.not.empty;
+        });
+
+        it('should return an event with an id of ev1', async () => {
+            const { eventManager } = await deployContractFixture();
+            await eventManager.createEvent(eventName, ticketLimit, priceInEther, timestampInFuture);
+            const eventsIdList = await eventManager.listEvents()
+            expect(eventsIdList[0]).to.be.equal("ev1");
+        });
+    });
+
+    describe('showEventDetails', async () => {
+        it('should return a tuple with the event details', async () => {
+
+            const { eventManager, owner } = await deployContractFixture();
+            await eventManager.createEvent(eventName, ticketLimit, priceInEther, timestampInFuture);
+            const [_eventId, _creator, _name, _ticketLimit, _price, _eventDate, _status] = await eventManager.showEventDetails("ev1");
+            const priceInWei = hre.ethers.parseEther(priceInEther.toString());
+
+            expect(_eventId).to.be.equal("ev1");
+            expect(_creator).to.be.equal(owner);
+            expect(_name).to.be.equal(eventName);
+            expect(_ticketLimit).to.be.equal(ticketLimit);
+            expect(_price).to.be.equal(priceInWei);
+            expect(_eventDate).to.be.equal(timestampInFuture);
+            expect(_status).to.equal(0);
+        });
+    });
+
+    describe('listEventParticipants', async () => {
+        it('should produce an error when an ID is not provided', async () => {
+            const { eventManager } = await deployContractFixture();
+            await expect(eventManager.listEventParticipants("")).to.be.revertedWith("Event ID cannot be empty");
+        });
+
+        it('should return an array with participants', async () => {
+            const { eventManager, addr1 } = await deployContractFixture();
+            await eventManager.createEvent(eventName, ticketLimit, priceInEther, timestampInFuture);
+            await eventManager.connect(addr1).buyTicket(eventId, fname, lname, email, { value: hre.ethers.parseEther("2") });
+            const participants = await eventManager.listEventParticipants("ev1");
+            expect(participants).to.not.be.empty;
+        });
+    });
+
+    describe('togglePauseEventRegistration', async () => {
+        it('should produce an error when an event doesnt exist with a specific ID', async () => {
+            const { eventManager } = await deployContractFixture();
+            await expect(eventManager.togglePauseEventRegistration("wrong-id", 1)).to.be.revertedWith("No event with that ID was found");
+        });
+
+        it('should produce an error when the event status is already set to the requested update', async () => {
+            const { eventManager } = await deployContractFixture();
+            const newStatus = 0; // Enum: 0 = Active (Standard value), 1 = Paused
+            await eventManager.createEvent(eventName, ticketLimit, priceInEther, timestampInFuture);
+            await expect(eventManager.togglePauseEventRegistration("ev1", newStatus)).to.be.revertedWith("That status is already set");
+        });
+
+        it('should update the status of the event to 1 (Paused)', async () => {
+            const { eventManager } = await deployContractFixture();
+            const newStatus = 1; // Enum: 0 = Active (Standard value), 1 = Paused
+            await eventManager.createEvent(eventName, ticketLimit, priceInEther, timestampInFuture);
+            await expect(eventManager.togglePauseEventRegistration("ev1", newStatus)).to.emit(eventManager, "EventStatusUpdated");
+        });
+    });
+
+    describe('buyTicket', async () => {
+        let snapshotId: any;
+
+        beforeEach(async () => {
+            snapshotId = await network.provider.request({
+                method: "evm_snapshot",
+                params: []
+            });
+        });
+
+        afterEach(async () => {
+            await network.provider.request({
+                method: "evm_revert",
+                params: [snapshotId]
+            });
+        });
+
+        it('should produce an error when no event was found with the provided ID', async () => {
+            const { eventManager, addr1 } = await deployContractFixture();
+            await expect(eventManager.connect(addr1).buyTicket("1337", fname, lname, email, { value: hre.ethers.parseEther("2") })).to.be.revertedWith("No event with that ID was found!");
+        });
+
+        it('should produce an error if the organier tries to buy a ticket to its own event', async () => {
+            const { eventManager } = await deployContractFixture();
+            await eventManager.createEvent(eventName, ticketLimit, priceInEther, timestampInFuture);
+            await expect(eventManager.buyTicket(eventId, fname, lname, email, { value: hre.ethers.parseEther("2") })).to.be.revertedWith("You cannot buy a ticket for your own event");
+        });
+
+        it('should produce an error if user tries to buy another ticket for same event', async () => {
+            const { eventManager, addr1 } = await deployContractFixture();
+            await eventManager.createEvent(eventName, ticketLimit, priceInEther, timestampInFuture);
+            await eventManager.connect(addr1).buyTicket(eventId, fname, lname, email, { value: hre.ethers.parseEther("2") });
+            await expect(eventManager.connect(addr1).buyTicket(eventId, fname, lname, email, { value: hre.ethers.parseEther("2") })).to.be.revertedWith("You have already purchased a ticket. You cannot buy another one..");
+        });
+
+        it('should produce an error if user tries to buy a ticket after the event has ended', async () => {
+            const timestampAddition = 1695128940;
+            const { eventManager, addr1 } = await deployContractFixture();
+            await eventManager.createEvent(eventName, ticketLimit, priceInEther, timestampInFuture);
+
+            // Move time into the future of the events timestamp
+            await hre.network.provider.request({
+                method: "evm_setNextBlockTimestamp",
+                params: [timestampInFuture + timestampAddition]
+            });
+            await hre.network.provider.send('evm_mine');
+
+            await expect(eventManager.connect(addr1).buyTicket(eventId, fname, lname, email, { value: hre.ethers.parseEther("2") })).to.be.revertedWith("Too late to buy a ticket! Better luck next time.");
+        });
+
+        it('should produce an error if user tries to buy a ticket after the event has been paused', async () => {
+            const { eventManager, addr1 } = await deployContractFixture();
+            await eventManager.createEvent(eventName, ticketLimit, priceInEther, timestampInFuture);
+            await eventManager.togglePauseEventRegistration(eventId, 1); // Enum: 0 = Active (Standard value), 1 = Paused
+            await expect(eventManager.connect(addr1).buyTicket(eventId, fname, lname, email, { value: hre.ethers.parseEther("2") })).to.be.revertedWith('The ticket sales has been paused. Please try again later.');
+        });
+
+        it('should produce an error if user tries to buy a ticket after the event has been sold out', async () => {
+            const { eventManager, addr1 } = await deployContractFixture();
+            await eventManager.createEvent(eventName, 0, priceInEther, timestampInFuture);
+            await expect(eventManager.connect(addr1).buyTicket(eventId, fname, lname, email, { value: hre.ethers.parseEther("2") })).to.be.revertedWith("Tickets are sold out");
+        });
+
+        it('should produce an error if user tries to buy a ticket with an insufficient balance', async () => {
+            const { eventManager, addr1 } = await deployContractFixture();
+            await eventManager.createEvent(eventName, ticketLimit, priceInEther, timestampInFuture);
+            await expect(eventManager.connect(addr1).buyTicket(eventId, fname, lname, email, { value: hre.ethers.parseEther("0.5") })).to.be.revertedWith("Not enough funds to buy a ticket! Refill account and please try again.");
+        });
+
+        it('should emit "NewTicketCreated" when a ticket was bought', async () => {
+            const { eventManager, addr1 } = await deployContractFixture();
+            await eventManager.createEvent(eventName, ticketLimit, priceInEther, timestampInFuture);
+            await expect(eventManager.connect(addr1).buyTicket(eventId, fname, lname, email, { value: hre.ethers.parseEther("2") })).to.emit(eventManager, "NewTicketCreated");
+        });
+
+        it('should refund any excess funds to the user', async () => {
+            const { eventManager, addr1 } = await deployContractFixture();
+            await eventManager.createEvent(eventName, ticketLimit, priceInEther, timestampInFuture);
+
+            const initialBalance = await ethers.provider.getBalance(addr1);
+
+            const tx = await eventManager.connect(addr1).buyTicket(
+                eventId, fname, lname, email, { value: hre.ethers.parseEther("10") } // Overpay
+            );
+
+            const receipt = await tx.wait();
+            const totalGasPrice = receipt.gasUsed * receipt.gasPrice;
+            const paidTicketPrice = hre.ethers.parseEther(priceInEther.toString());
+            const totalTicketCost = paidTicketPrice + totalGasPrice;
+            const expectedBalance = initialBalance - totalTicketCost;
+
+            expect(expectedBalance).to.be.equal(await ethers.provider.getBalance(addr1));
+
+        });
+
+    });
+
+    describe('getUserTicket', async () => {
+        it('should return false when no event ID is found', async () => {
+            const { eventManager, addr1 } = await deployContractFixture();
+            const [ticketExist] = await eventManager.connect(addr1).getUserTicket(eventId);
+            expect(ticketExist).to.be.false;
+        });
+
+        it('should return a ticket for an event', async () => {
+            const { eventManager, addr1 } = await deployContractFixture();
+            await eventManager.createEvent(eventName, ticketLimit, priceInEther, timestampInFuture);
+            const tx = await eventManager.connect(addr1).buyTicket(eventId, fname, lname, email, { value: hre.ethers.parseEther("2") });
+            const blockNumber = tx.blockNumber;
+            const block = await hre.ethers.provider.getBlock(blockNumber);
+            const [, ticketData] = await eventManager.connect(addr1).getUserTicket(eventId);
+
+
+            expect(ticketData.owner).to.be.equal(addr1);
+            expect(ticketData.fname).to.be.equal(fname);
+            expect(ticketData.lname).to.be.equal(lname);
+            expect(ticketData.email).to.be.equal(email);
+            expect(ticketData.paidPrice).to.be.equal(hre.ethers.parseEther(priceInEther.toString()));
+            expect(ticketData.purchased).to.be.equal(block.timestamp);
+
+        });
+    });
+
+    describe('cancelTicket', async () => {
+        let snapshotId: any;
+
+        beforeEach(async () => {
+            snapshotId = await network.provider.request({
+                method: "evm_snapshot",
+                params: []
+            });
+        });
+
+        afterEach(async () => {
+            await network.provider.request({
+                method: "evm_revert",
+                params: [snapshotId]
+            });
+        });
+
+        it('should revert when no ticket was found', async () => {
+            const { eventManager, addr1 } = await deployContractFixture();
+            await expect(eventManager.connect(addr1).cancelTicket(eventId)).to.be.revertedWithCustomError(eventManager, "NoTicketFound");
+        });
+
+        it('should provide an error message when passed the refund date', async () => {
+            const refundTimestamp = 1758232800;
+            const { eventManager, addr1 } = await deployContractFixture();
+            await eventManager.createEvent(eventName, ticketLimit, priceInEther, timestampInFuture);
+            await eventManager.connect(addr1).buyTicket(eventId, fname, lname, email, { value: hre.ethers.parseEther("2") });
+
+            // Move time into the future of the events timestamp
+            await hre.network.provider.request({
+                method: "evm_setNextBlockTimestamp",
+                params: [refundTimestamp]
+            });
+            await hre.network.provider.send('evm_mine');
+
+            expect(eventManager.connect(addr1).cancelTicket(eventId)).to.be.revertedWith("Sorry, the last date for a refund have passed :(");
+
+        });
+
+
+        it('should refund the ticket price and emit "TicketCanceled"', async () => {
+            const { eventManager, addr1 } = await deployContractFixture();
+            await eventManager.createEvent(eventName, ticketLimit, priceInEther, timestampInFuture);
+            await eventManager.connect(addr1).buyTicket(eventId, fname, lname, email, { value: hre.ethers.parseEther("2") });
+            const [, ticketData] = await eventManager.connect(addr1).getUserTicket(eventId);
+
+            const initialBalance = await ethers.provider.getBalance(addr1);
+            const tx = await eventManager.connect(addr1).cancelTicket(eventId);
+            const receipt = await tx.wait();
+            const totalGasPrice = receipt.gasUsed * receipt.gasPrice;
+            const expectedBalance = initialBalance + ticketData.paidPrice - totalGasPrice;
+
+            expect(expectedBalance).to.be.equal(await ethers.provider.getBalance(addr1));
+            expect(tx).to.emit(eventManager, "TicketCanceled");
+        });
+
+    });
+
+    describe('getContractBalance', async () => {
+        it('should return the contract balance of 0', async () => {
+            const { eventManager } = await deployContractFixture();
+            expect(await eventManager.getContractBalance()).to.be.equal(0);
+        });
+
+        it('should return the contract balance of 1 ether in wei', async () => {
+            const tempTicketPriceInEther = 1;
+            const { eventManager, addr1 } = await deployContractFixture();
+            await eventManager.createEvent(eventName, ticketLimit, tempTicketPriceInEther, timestampInFuture);
+            await eventManager.connect(addr1).buyTicket(eventId, fname, lname, email, { value: hre.ethers.parseEther("2") });
+
+            expect(await eventManager.getContractBalance()).to.be.equal(hre.ethers.parseEther(tempTicketPriceInEther.toString()));
+        });
+    });
+
+    describe('fallback', async () => {
+        it('should emit the correct log when fallback function is called', async () => {
+            const { eventManager, addr1 } = await deployContractFixture();
+            await expect(
+                addr1.sendTransaction({
+                    to: eventManager.getAddress(),
+                    data: "0x"  // Empty data triggers the fallback function
+                })
+            ).to.emit(eventManager, "Log");
+        });
+    });
+
+});
